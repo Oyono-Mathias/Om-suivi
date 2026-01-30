4c45398584b0934d66c96d43e82b96920791b00e
Mathias Oyono <mathiasoyono@gmail.com>
2026-01-30 17:28:25 +0000

    Regarde la possition de chargement

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -5,7 +5,7 @@ import { useTranslations } from "next-intl";
 export default function Loading() {
   const t = useTranslations("Shared");
   return (
-    <div className="flex flex-col justify-center items-center h-screen gap-4">
+    <div className="flex flex-col justify-center items-center h-full gap-4">
       <Loader2 className="h-16 w-16 animate-spin" />
       <p>{t("loading")}</p>
     </div>
