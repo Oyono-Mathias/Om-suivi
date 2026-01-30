7398b32cd085a1199a9a485b6502964aa1d789bb
Mathias Oyono <mathiasoyono@gmail.com>
2026-01-30 19:25:20 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:777892671254:web:08d8a34851c7a749b3aa37",
   "apiKey": "AIzaSyClosHZ730ak_5a0Y0nlV7aU9TXqWANoO0",
   "authDomain": "studio-3929362902-1f7dc.firebaseapp.com",
+  "storageBucket": "studio-3929362902-1f7dc.appspot.com",
   "measurementId": "",
   "messagingSenderId": "777892671254"
 };
