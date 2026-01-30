432d6a2b303cb9e8cb200614ec248db21d8e1f40
Mathias Oyono <mathiasoyono@gmail.com>
2026-01-30 17:21:57 +0000

    La page rapport quand je l'ouvre ça zoom sur mobile je suis obliger de d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang={locale} suppressHydrationWarning className={poppins.variable}>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#00D68F" />
       </head>
