dd845174977f98baa0dbaf08ec96c8bc8005556f
Mathias Oyono <mathiasoyono@gmail.com>
2026-02-23 07:11:22 +0000

    08:08:37.937 Running build in Washington, D.C., USA (East) – iad1 08:08:

---
+++ src/next.config.ts
@@ -0,0 +1,40 @@
+
+import type {NextConfig} from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+import withPWA from '@ducanh2912/next-pwa';
+
+const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
+
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default pwaConfig(withNextIntl(nextConfig));
